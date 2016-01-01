2a14d43c0988d1bb603ead0e1caeb3cf74a624e3
Schahriar SaffarShargh <info@schahriar.com>
2016-01-01 13:18:11 -0800

    Added init tests

---
+++ tests/main.js
@@ -0,0 +1,16 @@
+var chai = require("chai");
+var expect = chai.expect;
+var inspect = require("util").inspect;
+
+var Cluster = require('../cluster');
+var cluster;
+
+describe("Init Test Suite", function() {
+    it('should construct an instance', function() {
+        cluster = new Cluster();
+    });
+    it('should deploy a worker per CPU core', function() {
+        cluster.deploy();
+        expect(cluster.totalWorkers()).to.be.gte(4);
+    });
+});
