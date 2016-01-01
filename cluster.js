15aa4824b1f6fa4ea44bf2a4564f091119898f99
Schahriar SaffarShargh <info@schahriar.com>
2016-01-01 13:53:30 -0800

    Improved Cluster creation using .setupMaster
    Worker was separated to a new Worker.js file and master is directly
    managed under cluster.js

--- cluster.js
+++ cluster.js
@@ -12,30 +12,38 @@ var SuperTask = require('supertask');
 function noop() { return null; }
 
 var SuperTaskCluster = function STC_INIT() {
-    this._cluster = {};
+    this._st = null;
 };
 
 SuperTaskCluster.prototype.deploy = function STC_DEPLOY_CLUSTER() {
     var cores = os.cpus().length;
-    if (cluster.isMaster) {
-        // Create new master Supertask instance
-        this._st = new SuperTask();
-        // Fork workers.
-        for (var i = 0; i < cores; i++) {
-            var fork = cluster.fork();
-            this._cluster[fork.process.pid] = fork;
-        }
-
-        cluster.on('exit', function(worker, code, signal) {
-            console.log('worker ' + worker.process.pid + ' died');
-        });
-    } else {
-        // Worker //
+    
+    cluster.setupMaster({
+        exec: './lib/Worker.js',
+        args: [],
+        silent: true
+    });
+    // Fork workers.
+    for (var i = 0; i < cores; i++) {
+        cluster.fork();
     }
+    // Create new master Supertask instance
+    this._st = new SuperTask();
+    
+    // Listen
+    Object.keys(cluster.workers).forEach(function(id) {
+        cluster.workers[id].on('message', function() {
+            console.log("Message from", id);
+        });
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
 };
 
 SuperTaskCluster.prototype.getWorkers = function STC_GET_WORKERS() {
-    return this._cluster;
+    return cluster.workers;
 };
 
 SuperTaskCluster.prototype.totalWorkers = function STC_TOTAL_WORKERS() {
