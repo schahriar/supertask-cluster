51ccab88a53525a58db2eec63f61fe40a134362e
Schahriar SaffarShargh <info@schahriar.com>
2016-01-04 16:22:43 -0800

    Re-arranged Documentation to a new folder

--- docs.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var fs = require('fs');
-var render = require('jsdoc-to-markdown');
-var stream = fs.createWriteStream("./api.md");
-render({
-    src: __dirname + '/cluster.js',
-    separators: true
-}).pipe(stream);
